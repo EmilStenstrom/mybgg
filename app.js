e23ce7c732eb79ccee6186897ad410723549b44e
Emil Stenström <em@kth.se>
2018-05-10 22:09:28 +0200

    Initial commit.
    Simplify python import by removing "js" from name.
    
    Instead of being clever, use JSON.
    
    Call file .js to avoid GitHub's strict file limits.
    
    Load the JSON config properly from JS. Remove .sample.

---
+++ app.js
@@ -0,0 +1,174 @@
+function loadJSON(path, callback) {
+  var req = new XMLHttpRequest();
+  req.overrideMimeType("application/json");
+  req.open('GET', path, true);
+  req.onreadystatechange = function () {
+    if (req.readyState == 4 && req.status == "200") {
+      callback(JSON.parse(req.responseText));
+    }
+  };
+  req.send(null);
+}
+
+function init(SETTINGS) {
+  const search = instantsearch({
+    appId: SETTINGS.algolia.app_id,
+    apiKey: SETTINGS.algolia.api_key_search_only,
+    indexName: SETTINGS.algolia.index_name,
+    routing: true
+  });
+
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-box',
+      placeholder: 'Search for games'
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.clearAll({
+      container: '#clear-all',
+      templates: {
+        link: 'Clear all'
+      },
+      clearsQuery: true,
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#facet-categories',
+      collapsible: true,
+      attributeName: 'categories',
+      operator: 'and',
+      showMore: true,
+      templates: {
+        header: 'Categories'
+      }
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#facet-mechanics',
+      collapsible: true,
+      attributeName: 'mechanics',
+      operator: 'and',
+      showMore: true,
+      templates: {
+        header: 'Mechanics'
+      }
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.hierarchicalMenu({
+      container: '#facet-players',
+      collapsible: true,
+      attributes: ['players.level1', 'players.level2'],
+      operator: 'or',
+      showMore: true,
+      sortBy: function(a, b){ return parseInt(a.name) > parseInt(b.name) },
+      templates: {
+        header: 'Number of players'
+      }
+    })
+  );
+
+  WEIGHT_LABELS = [
+    "Light",
+    "Light Medium",
+    "Medium",
+    "Medium Heavy",
+    "Heavy"
+  ]
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#facet-weight',
+      collapsible: true,
+      attributeName: 'weight',
+      operator: 'or',
+      sortBy: function(a, b){ return WEIGHT_LABELS.indexOf(a.name) > WEIGHT_LABELS.indexOf(b.name) },
+      templates: {
+        header: 'Complexity'
+      }
+    })
+  );
+
+  PLAYING_TIME_ORDER = [
+    '< 30min',
+    '30min - 1h',
+    '1-2h',
+    '2-3h',
+    '3-4h',
+    '> 4h'
+  ]
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#facet-playing-time',
+      collapsible: true,
+      attributeName: 'playing_time',
+      operator: 'or',
+      sortBy: function(a, b){ return PLAYING_TIME_ORDER.indexOf(a.name) > PLAYING_TIME_ORDER.indexOf(b.name) },
+      templates: {
+        header: 'Playing time'
+      }
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      collapsible: true,
+      transformData: {
+        item: function(game){
+          num_players_list = [];
+          for (let num_players of game.players) {
+            num_players = num_players.level2.replace(/^\d+ > [\w ]+ (\d+\+?)$/, '$1')
+            num_players_list.push(num_players);
+
+            if (num_players.indexOf("+") > -1) {
+              break;
+            }
+          }
+          game.players = num_players_list.join(", ");
+
+          game.categories = game.categories.join(", ");
+          game.mechanics = game.mechanics.join(", ");
+          game.description = game.description.trim();
+          return game;
+        },
+      },
+      templates: {
+        empty: 'No results',
+        item: document.getElementById('hits-template').innerHTML
+      },
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.stats({
+      container: '#stats'
+    })
+  );
+
+  search.addWidget(
+    instantsearch.widgets.pagination({
+      container: '#pagination',
+      maxPages: 20
+    })
+  );
+
+  search.start();
+
+  function set_bgg_name() {
+    name = SETTINGS.boardgamegeek.user_name + "'s";
+    title = document.getElementsByTagName("title")[0];
+    title.innerHTML = title.innerHTML.replace("my", name);
+    h1 = document.getElementsByTagName("h1")[0];
+    h1.innerHTML = h1.innerHTML.replace("my", name);
+  }
+  set_bgg_name();
+}
+
+loadJSON("config.json", init);
---
+++ download_and_index.py
@@ -0,0 +1,174 @@
+import json
+import math
+from collections import namedtuple
+from textwrap import dedent
+
+from algoliasearch import algoliasearch
+from boardgamegeek import BGGClient
+from boardgamegeek.cache import CacheBackendSqlite
+
+SETTINGS = json.load(open("config.json", "rb"))
+
+class BoardGame:
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+class Downloader():
+    def __init__(self):
+        project_name = SETTINGS["project"]["name"]
+        self.client = BGGClient(
+            cache=CacheBackendSqlite(path=f"{project_name}-cache.sqlite", ttl=60*60*24)
+        )
+
+    def collection(self, user_name):
+        collection = self.client.collection(
+            user_name=user_name,
+            exclude_subtype=u'boardgameexpansion',
+            **SETTINGS["boardgamegeek"]["extra_params"],
+        )
+
+        games = [
+            self.game(game_in_collection.id)
+            for game_in_collection in collection.items
+        ]
+
+        # Print linebreak to terminal, since each game has print(..., end="")
+        print()
+
+        return games
+
+    def _num_players_is_recommended(self, num, votes):
+        if int(votes['best_rating']) + int(votes['recommended_rating']) > int(votes['not_recommended_rating']):
+            return True
+
+        return False
+
+    def _facet_for_num_player(self, num, num_with_maybe_plus, votes):
+        if int(votes['best_rating']) > 10 and int(votes['best_rating']) > int(votes['recommended_rating']):
+            best_or_recommended = f"{num} > Best with {num_with_maybe_plus}"
+        else:
+            best_or_recommended = f"{num} > Recommended with {num_with_maybe_plus}"
+
+        return {
+            "level1": num,
+            "level2": best_or_recommended,
+        }
+
+    def game(self, game_id):
+        game = self.client.game(game_id=game_id)
+
+        num_players = []
+        for num, votes in game.suggested_players['results'].items():
+            if not self._num_players_is_recommended(num, votes):
+                continue
+
+            if "+" not in num:
+                num_players.append(self._facet_for_num_player(num, num, votes))
+            else:
+                for i in range(int(num.replace("+", "")) + 1, 11):
+                    num_players.append(self._facet_for_num_player(i, num, votes))
+
+        playing_time_mapping = {
+            30: '< 30min',
+            60: '30min - 1h',
+            120: '1-2h',
+            180: '2-3h',
+            240: '3-4h',
+        }
+        for playing_time_max, playing_time in playing_time_mapping.items():
+            if playing_time_max > int(game.playing_time):
+                break
+        else:
+            playing_time = '> 4h'
+
+        weight_mapping = {
+          0: "Light",
+          1: "Light",
+          2: "Light Medium",
+          3: "Medium",
+          4: "Medium Heavy",
+          5: "Heavy",
+        }
+        weight = weight_mapping[math.ceil(game.rating_average_weight)]
+
+        # Print progress to terminal
+        print(".", end="", flush=True)
+
+        return BoardGame(
+            id=game.id,
+            name=game.name,
+            description=game.description,
+            image=game.thumbnail,
+            categories=[cat for cat in game.categories],
+            mechanics=[mec for mec in game.mechanics],
+            players=num_players,
+            weight=weight,
+            playing_time=playing_time,
+        )
+
+class Indexer:
+    def __init__(self, api_key_admin):
+        client = algoliasearch.Client(
+            app_id=SETTINGS["algolia"]["app_id"],
+            api_key=api_key_admin,
+        )
+        index = client.init_index(SETTINGS["algolia"]["index_name"])
+
+        index.set_settings({
+            'searchableAttributes': [
+                'name',
+                'description',
+            ],
+            'attributesForFaceting': [
+                'categories',
+                'mechanics',
+                'players',
+                'weight',
+                'playing_time',
+            ],
+            'highlightPreTag': '<strong class="highlight">',
+            'highlightPostTag': '</strong>'
+        })
+
+        self.index = index
+
+    def add_objects(self, collection):
+        games = [game.__dict__ for game in collection]
+        for game in games:
+            game["objectID"] = f"bgg{game['id']}"
+
+        self.index.add_objects(games)
+
+    def delete_objects_not_in(self, collection):
+        delete_filter = " AND ".join([f"id != {game.id}" for game in collection])
+        self.index.delete_by({
+            'filters': delete_filter,
+        })
+
+def main(api_key_admin):
+    downloader = Downloader()
+    collection = downloader.collection(
+        user_name=SETTINGS["boardgamegeek"]["user_name"]
+    )
+    print(f"Imported {len(collection)} games from boardgamegeek.")
+
+    indexer = Indexer(api_key_admin=api_key_admin)
+    indexer.add_objects(collection)
+    indexer.delete_objects_not_in(collection)
+    print(f"Indexed {len(collection)} games in algolia, and removed everything else.")
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Download and index some boardgames')
+    parser.add_argument(
+        '--apikey',
+        type=str,
+        required=True,
+        help='The admin api key for your algolia site'
+    )
+
+    args = parser.parse_args()
+
+    main(api_key_admin=args.apikey)
